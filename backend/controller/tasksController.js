b16c7ee781ac299411de4f75ea1ad9224f1f27c7
tobiasmesa <tobiasmesa030303@gmail.com>
2022-10-22 13:07:33 -0300

    Controller taksks and routes tasks setup

--- backend/controller/tasksController.js
+++ backend/controller/tasksController.js
@@ -1,17 +1,16 @@
-
+const asyncHandler = require('express-async-handler')
 //@desc Get task
 //@route GET /api/task 
 //@access Private
-const getTask = (req, res) => 
+const getTask = asyncHandler( async (req, res) => 
 {
-
     res.status(200).json({ message: 'Get Tasks!'});
-}
+})
 
 //@desc Set task
 //@route Post /api/task 
 //@access Private
-const setTask = (req, res) => 
+const setTask = asyncHandler ( async (req, res) => 
 {
    // console.log(req.body.text)
     if(!req.body.text)
@@ -20,24 +19,24 @@ const setTask = (req, res) =>
         throw new Error('Please add a text field!')
     }
     res.status(200).json({ message: 'Create task!'});
-}
+})
 
 //@desc Update task
 //@route PUT /api/task/:id
 //@access Private
-const updateTask = (req, res) => 
+const updateTask = asyncHandler ( async (req, res) => 
 {
     res.status(200).json({ message: `Update task ${req.params.id}`});
-}
+})
 
 //@desc Delete task
 //@route DELETE /api/task/:id
 //@access Private 
 
-const deleteTask = (req, res) => 
+const deleteTask = asyncHandler ( async (req, res) => 
 {
     res.status(200).json({ message: `Delete task ${req.params.id}`});
-}
+})
 
 module.exports = 
 {
