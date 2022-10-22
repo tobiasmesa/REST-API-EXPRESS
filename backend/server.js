dcabdac430129a5ebb1ffa3de14b6738ae599394
tobiasmesa <tobiasmesa030303@gmail.com>
2022-10-22 13:32:07 -0300

    Conection to DB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 /* Imports  */
 const express =  require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
-
-
+const connectDB = require('./config/db')
 const port = process.env.PORT || 3000 // Just in case
 
+
+connectDB()
 const app = express() 
 
 
