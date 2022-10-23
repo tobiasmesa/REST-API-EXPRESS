97ce7d8d09b9e7e485d12956e2f4672b57ae6540
tobiasmesa <tobiasmesa030303@gmail.com>
2022-10-22 23:37:40 -0300

    API Basic usages created

--- backend/controller/tasksController.js
+++ backend/controller/tasksController.js
@@ -1,24 +1,35 @@
 const asyncHandler = require('express-async-handler')
+
+const Task = require('../models/taskModel')
+
+
 //@desc Get task
 //@route GET /api/task 
 //@access Private
 const getTask = asyncHandler( async (req, res) => 
 {
-    res.status(200).json({ message: 'Get Tasks!'});
+    const tasks = await Task.find()
+    res.status(200).json(tasks);
 })
 
 //@desc Set task
 //@route Post /api/task 
 //@access Private
-const setTask = asyncHandler ( async (req, res) => 
+const setTask = asyncHandler ( async ( req, res ) => 
 {
-   // console.log(req.body.text)
+    //console.log(req.body.text)
+
     if(!req.body.text)
     {
         res.status(400)
         throw new Error('Please add a text field!')
     }
-    res.status(200).json({ message: 'Create task!'});
+
+    const task = await Task.create({
+        text: req.body.text
+    })
+
+     res.status(200).json(task);
 })
 
 //@desc Update task
@@ -26,7 +37,17 @@ const setTask = asyncHandler ( async (req, res) =>
 //@access Private
 const updateTask = asyncHandler ( async (req, res) => 
 {
-    res.status(200).json({ message: `Update task ${req.params.id}`});
+    const task = await Task.findById(req.params.id)
+
+    if(!task)
+    {
+        res.status(400)
+        throw new Error('Task not found!')
+    }
+
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+    res.status(200).json(updatedTask);
 })
 
 //@desc Delete task
@@ -35,7 +56,16 @@ const updateTask = asyncHandler ( async (req, res) =>
 
 const deleteTask = asyncHandler ( async (req, res) => 
 {
-    res.status(200).json({ message: `Delete task ${req.params.id}`});
+    const task = await Task.findById(req.params.id)
+
+    if(!task)
+    {
+        res.status(400)
+        throw new Error('Task not found!')
+    }
+
+    await task.remove()
+    res.status(200).json({ id: req.params.id });
 })
 
 module.exports = 
--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,12 @@ const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+
 const port = process.env.PORT || 3000 // Just in case
 
-
-connectDB()
 const app = express() 
 
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded( { extended: false } ))
