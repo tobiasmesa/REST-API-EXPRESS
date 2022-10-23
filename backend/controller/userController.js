147328e0b1caa8bf52182d417c41420204cb5ef4
tobiasmesa <tobiasmesa030303@gmail.com>
2022-10-23 20:15:51 -0300

    Protecting routes

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -44,7 +44,8 @@ const registerUser = asyncHandler (async (req, res) =>
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -57,18 +58,51 @@ const registerUser = asyncHandler (async (req, res) =>
 // @desc    Authenticate a user
 // @route   POST /api/users/login
 // @access  Public
-const loginUser =asyncHandler ( async (req, res) =>
+const loginUser = asyncHandler ( async (req, res) =>
 {
+    const { email, password } = req.body
+
+    // Check user email
+    const user = await User.findOne ({ email })
+
+    if(user && (await bcrypt.compare(password, user.password)))
+    {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        })
+    } else 
+    {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
     res.json({message: 'Login user'})
 })
 
 // @desc    Get user data
 // @route   GET /api/users/me
-// @access  Public
+// @access  Private
 const getMe = asyncHandler (async (req, res) =>
 {
-    res.json({message: 'User data display'})
+    const {_id, name, email} = await User.findById(req.user.id)
+    
+    res.status(200).json({
+        id: _id,
+        name,
+        email,
+    })
+
 })
+
+// Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+      expiresIn: '30d',
+    })
+  }
+
 
 
 module.exports = {
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,8 +1,10 @@
 const express =  require('express')
 const router = express.Router()
 const { getTask, setTask, updateTask, deleteTask} = require ('../controller/tasksController')
+const { protect } = require('../middleware/authMiddleware')
 
-router.route('/').get(getTask).post(setTask)
-router.route('/:id').put(updateTask).delete(deleteTask)
+
+router.route('/').get(protect, getTask).post(protect, setTask)
+router.route('/:id').put(protect, updateTask).delete(protect, deleteTask)
 
 module.exports = router
