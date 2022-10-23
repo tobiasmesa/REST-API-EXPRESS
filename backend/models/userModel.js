7770caff700583cb555d2cdcc49e716a62a5addd
tobiasmesa <tobiasmesa030303@gmail.com>
2022-10-23 15:12:35 -0300

    Now Users can register

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,27 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
-    {
-        name: {
-          type: String,
-          required: [true, 'Please add a name'],
-        },
-    }, {
-        email: {
-            type: String,
-            required: [true, 'Please add a email'],
-            unique: true
-          },
-    }, 
-    {
-        password: {
-            type: String,
-            required: [true, 'Please add a password'],
-          },
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
     },
-    {
-        timestamps: true
-    }
-)
+    email: {
+      type: String,
+      required: [true, "Please add a email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
